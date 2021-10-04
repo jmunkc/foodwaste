56bf85f1760aabd8e293e82defcd4d6dda9817f2
jmunkc <jona04g8@stud.kea.dk>
2021-10-04 16:07:17 +0200

    all pages linked with mapping

--- src/main/java/com/foodwaste/controllers/FoodWasteController.java
+++ src/main/java/com/foodwaste/controllers/FoodWasteController.java
@@ -10,4 +10,29 @@ public class FoodWasteController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/info")
+    public String info(){
+        return "info";
+    }
+
+    @GetMapping("/find-food")
+    public String findFood(){
+        return "find-food";
+    }
+
+    @GetMapping("/sign-up")
+    public String signUp(){
+        return "sign-up";
+    }
+
+    @GetMapping("/faq")
+    public String faq(){
+        return "faq";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
 }
