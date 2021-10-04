cd4ad4d198387ca2396db9ffa5092d31e8a96c57
jmunkc <jona04g8@stud.kea.dk>
2021-10-04 10:49:50 +0200

    First commit

---
+++ src/main/java/com/foodwaste/FoodwasteApplication.java
@@ -0,0 +1,13 @@
+package com.foodwaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodwasteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodwasteApplication.class, args);
+    }
+
+}
